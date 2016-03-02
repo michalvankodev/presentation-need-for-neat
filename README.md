1c1476751043371901fea8a19324bd440276906d
Michal Vanko <mklakakilli@gmail.com>
2016-03-02 16:15:40 +0100

    Intro page

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# presentation-need-for-neat
-The need for reactive with the neat of functional presentation by Michal Vanko
+# Need for neat
+
+### The need for reactive with the neat of functional presentation by Michal Vanko
+
+## Notes
+- History Lesson - Compare applications 200X - 2016
+- Compare testing pure function with classical functions
+- LiveCoding: Autocomplete search
+-
+
+## Slides
+1. Intro
+2. Overview
+3. History Lesson
+
+
+## Development
+To start development: `npm start`
+
+To create a gh-pages bundle: `npm run bundle`
--- src/app.js
+++ src/app.js
@@ -1,13 +1,27 @@
 import {Observable} from 'rx'
-import {div} from '@cycle/dom'
-
+import {div, h1, h3, span, section} from '@cycle/dom'
+import getRandomColor from './get-random-color'
 export default function app({DOM, HTTP}) {
 
+  const color$ = Observable.interval(500).map(i => getRandomColor())
+
+  let introPageView = color$.map(color =>
+    section('.intro', [
+      h1([
+        span('The need for '),
+        span('.reactive', { style: {'color': color} }, 'reactive'),
+        span(', the neat of '),
+        span('.functional', 'f('),
+        span('unctional'),
+        span('.functional', ')')
+      ]),
+      h3('by Michal Vanko')
+
+    ])
+  );
 
   return {
-    DOM: Observable.of(
-      div('aasssa')
-    ),
+    DOM: introPageView,
     HTTP: Observable.just({})
   }
 }
